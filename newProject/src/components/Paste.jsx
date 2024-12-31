d30a2b5d20c4cf1555b515cd646ca1c24d7077e2
Sumit <sumitkothari18@gmail.com>
2024-12-31 18:30:00 +0530

    Your commit message here

--- newProject/src/components/Paste.jsx
+++ newProject/src/components/Paste.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
+import { useDispatch, useSelector,} from 'react-redux'
 
 function Paste() {
+  const pastes=useSelector((state)=>state.paste.pastes)
+  const dispatch=useDispatch();
+  const [searchTerm,setSearchTerm]=useState('')
+  const filteredData=pastes.filter(
+    (paste)=>paste.title.toLowerCase().includes(searchTerm.toLowerCase())
+  )
   return (
-    <div>Paste</div>
+
+    <div>
+      <input className='p-2 rounded-2xl min-w-[600px] mt-4' type="search "
+      placeholder='Search Here' 
+      value={searchTerm}
+      onChange={(e)=>setSearchTerm(e.target.value)}/>
+
+      <div className='flex flex-col gap-5'>
+        {
+          filteredData.length >0 &&
+          filteredData.map(
+            (paste) => {return (
+              <div>
+                {paste.title}
+              </div>
+            )}
+          )
+          
+        }
+
+      </div>
+    </div>
   )
 }
 
